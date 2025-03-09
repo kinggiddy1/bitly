cb779588dbd8511bd0504633ab58e00eb7a7d4e9
kinggiddy1 <kinggiddy@gmail.com>
2025-03-09 13:33:12 +0200

    added authentication component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { DefaultLayoutComponent } from './home/default-layout/default-layout/default-layout.component';
 import { IndexComponent } from './home/index/index/index.component';
+import { AuthenticationComponent } from './home/authentication/authentication/authentication.component';
 
 export const routes: Routes = [
     {
@@ -12,6 +13,10 @@ export const routes: Routes = [
             component:IndexComponent,
             pathMatch: 'full',
           },
+          {
+            path: 'auth',
+            component:AuthenticationComponent ,
+          }
        
         ]
       },
