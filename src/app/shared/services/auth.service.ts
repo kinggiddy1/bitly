2cdb4d94cac06b7e796c75b4dde8c6fbe31f5cc4
unknown <kinggiddy@gmail.com>
2025-03-28 18:38:08 +0200

    changed environments

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment.development';
 import { Observable } from 'rxjs';
 import { TOKEN_KEY } from '../contants/constants';
 import { Router } from '@angular/router';
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
+    production: true,
     apiUrl: 'https://node-api.thefocalmedia.com/api/',
   };
